fb8d15e05febca86ff8e93e056758f66df4a53d5
efrensho <efrenziho@gmail.com>
2020-10-02 18:23:06 -0500

    Smallest Common Multiple challenge added

---
+++ smallest-common-multiple.js
@@ -0,0 +1,37 @@
+function smallestCommons(arr) {
+    let max = Math.max(...arr);
+    let multip = max;
+    let nums = [];
+
+    // define numbers in the range of the numbers in the arguments
+    for (let i = Math.min(...arr); i <= max; i++) {
+        nums.push(i);
+    }
+
+    // returns until it finds a number that satisfy the condition, that is, when is divisible by all numbers between the min and max.
+    while (!nums.every(n => multip % n == 0)) {
+        multip += max;
+    }
+
+    return multip;
+}
+
+
+
+// special metion JanEgbers from the freeCodeCamp forum who came up with this simple but amazing chunk of code 
+// and by which I was inspired to come up with my solution.
+function smallestCommons(arr) {
+
+    var max = Math.max(arr[0], arr[1]);
+    var min = Math.min(arr[0], arr[1]);
+    var mltple = max;
+
+    for (var i = max; i >= min; i--) {
+        if (mltple % i !== 0) {
+            mltple += max;
+            i = max;
+        }
+    }
+
+    return mltple;
+}
